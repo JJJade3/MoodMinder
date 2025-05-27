236bbc0b0b4a8f69ef71279543853a3d570f2029
Jade Li <zli@g-ai-n.com>
2025-05-27 16:01:41 -0400

    Initial Commit

---
+++ MoodMinder/MoodMinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMinderApp.swift
+//  MoodMinder
+//
+//  Created by Jade Li on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodMinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoodMinderTests/MoodMinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMinderTests.swift
+//  MoodMinderTests
+//
+//  Created by Jade Li on 5/27/25.
+//
+
+import Testing
+@testable import MoodMinder
+
+struct MoodMinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
